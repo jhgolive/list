aa8e0f7eaf5e9c6f0e80ea8c5cde5abf606bfca8
jhgolive <rock0424@hanmail.net>
2025-10-14 19:13:02 +0900

    Update server.js

--- server.js
+++ server.js
@@ -187,7 +187,7 @@ app.get("/nightbot", async (req, res) => {
     // =====================
     //const output = `${dateStr}\n\n${results.map(r => `📌 ${r.text.replace(/\n/g, " | ")}`).join(" — ")}`; // 이벤트 간 구분
     //const output = `${dateStr}\n\n${results.map(r => `📌 ${r.text.replace(/\n/g, "\n | ")}`).join("\n — ")}`; // 이벤트 간 구분
-    const output = `${dateStr}\n${results.map(r => `📌 ${r.text.trim().replace(/\n/g, "\n | ")}`).join("\n  \n")}`;
+    const output = `📌 ${dateStr}\n${results.map(r => `💥 ${r.text.trim().replace(/\n/g, "\n | ")}`).join("\n  \n")}`;
         
     //const output = `${dateStr}\n\n${results.map(r => r.text).join("\n")}`; 
     const result = output.length > 1500 ? output.slice(0, 1500) + "…(생략)" : output;
