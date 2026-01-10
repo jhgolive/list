7063814ab14ab0dbc5b5f90160dd19169ac5bdf0
jhgolive <rock0424@hanmail.net>
2026-01-10 19:18:58 +0900

    Update server.js

--- server.js
+++ server.js
@@ -310,8 +310,8 @@ app.get("/nightbot", async (req, res) => {
       const d = String(kst.getDate()).padStart(2, "0");
       const hh = String(kst.getHours()).padStart(2, "0");
       const mm = String(kst.getMinutes()).padStart(2, "0");
-      //out += `\n\n💫 ${y}-${m}-${d} ${hh}:${mm}  @쩡햄Live`;
-      out += `\n\n💫 ${m}-${d} ${hh}:${mm} 업데이트`;  // 연도 제거
+      //out += `\n\n💫 ${y}-${m}-${d} ${hh}:${mm} 업데이트`;
+      out += `\n\n💫 ${m}-${d} ${hh}:${mm}  @쩡햄Live`;  // 연도 제거
     }
 
     return res.type("text/plain").send(out);
