669835753c97852bae9168e7b007c831c80e5619
jhgolive <rock0424@hanmail.net>
2026-01-17 16:40:11 +0900

    Update server.js

--- server.js
+++ server.js
@@ -176,7 +176,7 @@ async function fetchEventsForDate(dateIso, datePretty) {
   if (!links.length) {
     const warningLine = isDateSum18(dateIso) ? "\n💢 재난 사고 조심 💢\n" : "\n";
 
-    const text = `✨ ${datePretty}  0건` + warningLine + `\n해당 날짜에 일정이 없습니다.\n\n💫 ${formatKSTTime()} ✨신규  @쩡햄Live`;
+    const text = `✨ ${datePretty}  0건` + warningLine + `\n해당 날짜에 일정이 없습니다.\n\n💫 ${formatKSTTime()} ✨신규 💢레드데이  @쩡햄Live`;
     
     cache.set(dateIso, { updated: Date.now(), full: text, chunks: [text], count: 0 });
     return;
@@ -272,7 +272,7 @@ async function fetchEventsForDate(dateIso, datePretty) {
   //const full = `✨ ${datePretty}  ${results.length}건\n\n${chunks.join("\n\n")}\n\n💫 ${updatedTime} ✨신규  @쩡햄Live`;
   const warningLine = isDateSum18(dateIso) ? "\n💢 재난 사고 조심 💢\n" : "\n";
 
-  const full = `✨ ${datePretty}  ${results.length}건` + warningLine + `\n${chunks.join("\n\n")}\n\n💫 ${updatedTime} ✨신규  @쩡햄Live`;
+  const full = `✨ ${datePretty}  ${results.length}건` + warningLine + `\n${chunks.join("\n\n")}\n\n💫 ${updatedTime} ✨신규 💢레드데이  @쩡햄Live`;
 
   cache.set(dateIso, {
     updated: Date.now(),
