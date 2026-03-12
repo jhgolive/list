5bb001a34c03924ca36cfc7394a9122a76c45bf3
jhgolive <rock0424@hanmail.net>
2026-03-12 17:21:17 +0900

    Update server.js

--- server.js
+++ server.js
@@ -180,7 +180,7 @@ async function fetchEventsForDate(dateIso, datePretty) {
 
     const warningLine = isDateSum18(dateIso) ? `💢 ${datePretty} 사고조심  0건` : `✨ ${datePretty}  0건`;
 
-    const text = warningLine + `\n\n   - 해당 날짜에 일정이 없습니다. -\n\n💫 ${formatKSTTime()} ✨신규 💢레드데이  @쩡햄Live`;
+    const text = warningLine + `\n\n   - 해당 날짜에 일정이 없습니다. -\n\n💫 ${formatKSTTime()} ✨신규 💢레드데이  🍖쩡햄Live`;
     
     cache.set(dateIso, { updated: Date.now(), full: text, chunks: [text], count: 0 });
     return;
@@ -279,7 +279,7 @@ async function fetchEventsForDate(dateIso, datePretty) {
 
   const warningLine = isDateSum18(dateIso) ? `💢 ${datePretty} 사고조심  ${results.length}건` : `✨ ${datePretty}  ${results.length}건`;
 
-  const full = warningLine + `\n\n${chunks.join("\n\n")}\n\n💫 ${updatedTime} ✨신규 💢레드데이  @쩡햄Live`;
+  const full = warningLine + `\n\n${chunks.join("\n\n")}\n\n💫 ${updatedTime} ✨신규 💢레드데이  🍖쩡햄Live`;
   
   cache.set(dateIso, {
     updated: Date.now(),
