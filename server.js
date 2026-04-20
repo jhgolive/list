caea0ebd4d3a0c5f679044b8e6bcf56ed2191102
jhgolive <rock0424@hanmail.net>
2026-04-21 04:50:58 +0900

    Update server.js

--- server.js
+++ server.js
@@ -316,7 +316,7 @@ async function fetchEventsForDate(dateIso, datePretty) {
       const warningLine = isDateSum18(dateIso) ? `💢 ${datePretty} 사고조심  0건` : `✨ ${datePretty}  0건`;
   
       //const text = warningLine + `\n\n   - 해당 날짜에 일정이 없습니다. -\n\n💫${formatKSTTime()} ✨신규 💢레드데이  🍖쩡햄Live`;
-      const text = warningLine + `\n\n` + weatherLine  + `   - 해당 날짜에 일정이 없습니다. -\n\n` + `💫${formatKSTTime()} ✨신규 💢레드데이` + `\n☀서울날씨 최저/최고 새벽/아침/낮/저녁/최대강수` + `  🍖쩡햄Live`;
+      const text = warningLine + `\n` + weatherLine  + `   - 해당 날짜에 일정이 없습니다. -\n\n` + `💫${formatKSTTime()} ✨신규 💢레드데이` + `\n☀서울 최저/최고 새벽/아침/낮/저녁/최대강수` + `  🍖쩡햄Live`;
       
       cache.set(dateIso, { updated: Date.now(), full: text, chunks: [text], count: 0 });
       return;
@@ -422,7 +422,7 @@ async function fetchEventsForDate(dateIso, datePretty) {
     const warningLine = isDateSum18(dateIso) ? `💢 ${datePretty} 사고조심  ${results.length}건` : `✨ ${datePretty}  ${results.length}건`;
   
     //const full = warningLine + `\n\n${chunks.join("\n\n")}\n\n💫${updatedTime} ✨신규 💢레드데이  🍖쩡햄Live`;
-    const full = warningLine + `\n\n` + weatherLine + `${chunks.join("\n\n")}\n\n` + `💫${updatedTime} ✨신규 💢레드데이` + `\n☀서울날씨 최저/최고 새벽/아침/낮/저녁/최대강수` + `  🍖쩡햄Live`;
+    const full = warningLine + `\n` + weatherLine + `${chunks.join("\n\n")}\n\n` + `💫${updatedTime} ✨신규 💢레드데이` + `\n☀서울 최저/최고 새벽/아침/낮/저녁/최대강수` + `  🍖쩡햄Live`;
     
     cache.set(dateIso, {
       updated: Date.now(),
